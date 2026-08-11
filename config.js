9a4d3535b77c9d3ae36798900993a46fd129df9e
امیرحسین سرافراز <sa0ar0m0@gmail.com>
2026-08-11 12:04:16 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// =========================================================
+// این فایل رو با اطلاعات پروژه‌ی Supabase خودت پر کن
+// Supabase Dashboard -> Project Settings -> API
+// =========================================================
+window.CYBER_GYM_CONFIG = {
+  SUPABASE_URL: "https://czazuqfvgdhexeseeome.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6YXp1cWZ2Z2RoZXhlc2Vlb21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMTA0MDksImV4cCI6MjEwMTc4NjQwOX0.HDFhcgVEjEWuJUrxirb8d7GrBoHO8s_DKvK2y0IWptY"
+};
